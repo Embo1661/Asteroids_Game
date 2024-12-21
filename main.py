c14a7afc1d2e655c19c2df2abcc5f75d3ef14a97
Embo1661 <emil.ehrnrooth@gmail.com>
2024-12-21 20:40:22 +0000

    CH2L6

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_WIDTH / 2)
     dt = 0
 
     while True:                                     #infined game loop
-        
+
         for event in pygame.event.get():            #pygame X (exit game)
             if event.type == pygame.QUIT:
                 return clock.tick(60)
 
+        player.update(dt)
         screen.fill("black")
-        pygame.display.flip()
+        player.draw(screen)
+        pygame.display.flip()                       #update game
 
         # limit the framerate to 60 fps
         dt = clock.tick(60) / 1000
