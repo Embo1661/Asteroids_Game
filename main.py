60087100c455716c87919d5a8bb4f2afc3b064e6
Embo1661 <emil.ehrnrooth@gmail.com>
2024-12-21 19:32:44 +0000

    newCH2L3

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:                                     #while True infined game loop
-        for event in pygame.event.get():
+        for event in pygame.event.get():            #pygame X (exit game)
             if event.type == pygame.QUIT:
-                return
+                return clock.tick(60)
+
         screen.fill("black")
         pygame.display.flip()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
